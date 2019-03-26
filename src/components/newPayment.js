660a62f74fc384d93957b06e9bcc194b4e81b288
Romanov Sergey <judge_exe@mail.ru>
2019-03-26 09:39:23 +0300

    payment component v1

---
+++ src/components/newPayment.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+
+class NewPayment extends Component {
+  state = { 
+      category: this.props.paymentCategories[0],
+      price: ''
+  }
+  
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+    const { price, category } = this.state
+    this.props.addPayment({
+        price: price || 0,
+        category
+    })
+    this.setState({ price: ''})
+  }
+
+  render() {
+    const { price, category } = this.state
+    return (
+        <form onSubmit={this.handleSubmit}>
+          <p>Select category and write price</p>
+          <select 
+            name="category"
+            onChange={this.handleChange} 
+            value={category}
+          >
+            {this.props.paymentCategories.map((item, index) => 
+              <option value={item} key={index}>
+                { item }
+              </option>)}
+          </select>
+          <input 
+            name="price" 
+            type='number' 
+            placeholder="Add price, $" 
+            onChange={this.handleChange} 
+            value={price}
+          />
+          <button type="submit">Save</button>
+        </form>
+    );
+  }
+}
+  
+function mapStateToProps(state){
+  return state.paymentReducer
+}
+
+function mapDispatchToProps(dispatch){
+  return{
+      addPayment:(payment) => {
+        dispatch(
+          {
+            type:"ADD_PAYMENT",
+            payload: payment
+          }
+        )
+      }
+  }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(NewPayment);
---
+++ src/components/paymentsList.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+
+class List extends Component {
+  render() {
+    return(
+      <div className="list">
+        {this.props.payments.map((item, index)=>
+          <p key={index}>
+              {`${item.price}, $`}  |  {item.category}
+          </p>)}
+      </div>
+     )
+  }
+}
+
+function mapStateToProps(state){
+  return state.paymentReducer
+}
+
+export default connect(mapStateToProps)(List);
