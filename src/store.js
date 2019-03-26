6670a4b589c504668a4ea1ff253de082b1d9804b
Romanov Sergey <judge_exe@mail.ru>
2019-03-26 11:33:46 +0300

    fix store const

--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 import { createStore } from 'redux';
 import combineReducers from './reducers/mainReducer';
 
-let store = createStore(combineReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+const store = createStore(combineReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 export default store
