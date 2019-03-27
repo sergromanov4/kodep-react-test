576fd5ae25058f5273d720d9ad585374f9edbdd0
Romanov Sergey <judge_exe@mail.ru>
2019-03-27 12:34:45 +0300

    modal done v1

--- src/components/settings/payment.js
+++ src/components/settings/payment.js
@@ -5,22 +5,44 @@ import Modal from 'react-modal'
 class Payment extends Component {
   state={
     isActive: false,
-    newCategory: ''
+    newCategory: '',
+    editCategory: '',
+    editIndex: 0,
+    edit: false
+  }
+
+  handeleSubmitNew = event => {
+    event.preventDefault()
+    const { newCategory } = this.state
+    if (newCategory) 
+      this.props.addPaymentCategory(newCategory) 
+    this.setState({ newCategory: "" })
+    this.toggleModal()
+  }
+
+  handeleSubmitEdit = event => {
+    event.preventDefault()
+    const { editCategory, editIndex } = this.state
+    this.props.editPaymentCategory({ category:editCategory,
+                                     index: editIndex })
+    this.toggleModal()
   }
 
   handleRemove = index => {
     this.props.removePaymentCategory(this.props.paymentCategories[index])
   }
 
-  handeleSubmit = event => {
-    event.preventDefault()
-    this.props.addPaymentCategory(this.state.newCategory)
-    this.setState({newCategory: ""})
-    this.toggleModal()
-  }
-
   toggleModal = () => {
-    this.setState({isActive: !this.state.isActive})
+    this.setState({ isActive: !this.state.isActive })
+    if (this.state.edit)
+      this.setState({ edit: false }) 
+  }
+  
+  toggleModalEdit = index => {
+    this.setState({ isActive: !this.state.isActive,
+                    edit: !this.state.edit,
+                    editCategory: this.props.paymentCategories[index],
+                    editIndex: index })
   }
 
   componentWillMount() {
@@ -31,8 +53,31 @@ class Payment extends Component {
     this.setState({ [event.target.name]: event.target.value })
   }
 
-
   render() {
+    const newForm = 
+      <form className="modal-form" onSubmit={this.handeleSubmitNew}>
+        <p>New category</p>
+        <input 
+          name="newCategory" 
+          placeholder="Add new category" 
+          value={this.state.newCategory} 
+          onChange={this.handleChange}
+        />
+        <button type="submit">Save</button>
+      </form>
+
+    const editForm = 
+      <form className="modal-form" onSubmit={this.handeleSubmitEdit}>
+        <p>Edit category</p>
+        <input 
+          name="editCategory" 
+          placeholder="Edit category" 
+          value={this.state.editCategory} 
+          onChange={this.handleChange}
+        />
+        <button type="submit">Save</button>
+      </form>
+
     return(
       <div className="settings">
         <h2> Payments settings</h2>
@@ -41,28 +86,23 @@ class Payment extends Component {
           <div className="setting-list" key={index}>
             <p>{item}</p>
             <div className='btn-box'>
-               <button>Edit</button>
-               <button onClick={this.handleRemove.bind(this,index)}>Delete</button>
+               <button 
+                 name="edit" 
+                 onClick={this.toggleModalEdit.bind(this, index)}
+              >
+                Edit
+              </button>
+               <button onClick={this.handleRemove.bind(this, index)}>Delete</button>
             </div>
           </div>
         )}
-
         <Modal 
           isOpen={this.state.isActive} 
           onRequestClose={this.toggleModal} 
           className="modal" 
           overlayClassName="modal-overlay"
         >
-        <form className="modal-form" onSubmit={this.handeleSubmit}>
-          <p>New category</p>
-          <input 
-            name="newCategory" 
-            placeholder="Add new category" 
-            value={this.state.newCategory} 
-            onChange={this.handleChange}
-          />
-          <button type="submit">Save</button>
-        </form>
+          {this.state.edit ? editForm : newForm}
         </Modal>
       </div> 
     )
@@ -90,8 +130,15 @@ function mapDispatchToProps(dispatch){
             payload: payment
           }
         )
+      },
+      editPaymentCategory:(category) => {
+        dispatch(
+          {
+            type: "EDIT_CATEGORY",
+            payload: category
+          }
+        )
       }
-
   }
 }
 
--- src/reducers/paymentReducer.js
+++ src/reducers/paymentReducer.js
@@ -8,12 +8,19 @@ function paymentReducer(state=initialState,action){
     case "ADD_PAYMENT":
       return {...state, payments:[...state.payments, action.payload]}
     case "ADD_NEW_CATEGORY":
-      return {...state, paymentCategories:[...state.paymentCategories, action.payload]}
+      return {...state, paymentCategories: [...state.paymentCategories, action.payload]}
+    case "EDIT_CATEGORY":
+      return {...state, 
+               paymentCategories: state.paymentCategories.map(
+                 (item,index) => index === action.payload.index ? 
+                   item = action.payload.category : item
+               )
+             }
     case "REMOVE_PAYMENT_CATEGORY":
-      return {...state, paymentCategories:state.paymentCategories.filter( item => item !== action.payload)}
+      return {...state, paymentCategories: state.paymentCategories.filter( item => item !== action.payload)}
     default:
       return state
- }
+  }
 }
 
 export default paymentReducer
